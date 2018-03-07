a86b7fa92709395dc596c42b7271761d3297f841
Simon Waight <simon.waight@kloud.com.au>
2018-03-06 22:35:37 -0800

    Shortened timer time.

---
+++ Siliconvalve.FunctionDemo01/TimerJobSample.cs
@@ -0,0 +1,36 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using SendGrid.Helpers.Mail;
+
+namespace Siliconvalve.FunctionDemo01
+{
+    public static class TimerJobSample
+    {
+        [FunctionName("TimerJobSample")]
+        public static void Run([TimerTrigger("*/10 * * * * *")]TimerInfo myTimer, [SendGrid(From = "%NotificationsSender%")] out SendGridMessage message, TraceWriter log)
+        {
+            log.Info($"C# Timer trigger function executed at: {DateTimeOffset.UtcNow}");
+
+            message = new SendGridMessage();
+
+            try
+            {
+                message.AddContent("text/plain", "I'll send an SOS to the world.");
+                message.AddTo(GetEnvironmentVariable("TimerRecipient"));
+                message.Subject = "Message in a Bottle.";
+
+                throw new PlatformNotSupportedException("Can't run, not supported");
+            }
+            catch (Exception ex)
+            {
+                log.Error("TimerJobSample unhandled Exception", ex);
+            }
+        }
+
+        private static string GetEnvironmentVariable(string name)
+        {
+            return Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.Process);
+        }
+    }
+}
